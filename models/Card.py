34735d2270ef2f52d81b78c043c66ca901dca88d
acortino <hello@acortino.me>
2020-05-22 16:50:29 +0200

    Add color recognition

--- models/Card.py
+++ models/Card.py
@@ -1,6 +1,14 @@
 # encoding: utf-8
 REF_VALUE_STR = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 REF_COLOR_STR = ["♠", "♣", "♡", "♢"]
+COLOR_BLACK = 0
+COLOR_GREEN = 1
+COLOR_RED = 2
+COLOR_BLUE = 3
+REF_COLOR_RGB_RED = [178, 7, 27]
+REF_COLOR_RGB_BLUE = [12, 49, 131]
+REF_COLOR_RGB_GREEN = [55, 187, 34]
+REF_COLOR_RGB_BLACK = [0, 0, 0]
 
 
 class Card:
--- models/OCR.py
+++ models/OCR.py
@@ -1,7 +1,8 @@
+import numpy as np
 from imutils import contours
 import cv2
-import numpy as np
-from models.Card import Card
+import operator
+from models.Card import Card, REF_COLOR_RGB_RED, REF_COLOR_RGB_BLACK, REF_COLOR_RGB_BLUE, REF_COLOR_RGB_GREEN
 import inspect
 
 
@@ -22,7 +23,7 @@ class OCR:
     def selectRegion(self):
         # Allow you to select a region on a picture and print the coordinate
         r = cv2.selectROI(self.image)
-        print r
+        print(r)
         # Crop image
         # [y:y+h, x:x+w]
         imCrop = self.image[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
@@ -30,7 +31,7 @@ class OCR:
     def setOCRReference(self):
         gray = cv2.cvtColor(self.reference, cv2.COLOR_BGR2GRAY)
         refThresh = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)[1]
-        im2, refCnts, hierarchy = cv2.findContours(refThresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        refCnts, hierarchy = cv2.findContours(refThresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         refCnts = contours.sort_contours(refCnts, method="left-to-right")[0]
         for (i, c) in enumerate(refCnts):
             (x, y, w, h) = cv2.boundingRect(c)
@@ -44,7 +45,7 @@ class OCR:
         img = self.selectMyHand()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         thresh = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)[1]
-        im2, cnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        cnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         cnts = contours.sort_contours(cnts, method="left-to-right")[0]
         locs = []
 
@@ -56,8 +57,6 @@ class OCR:
                 rect = (x, y, w, h)
                 locs.append(rect)
                 cv2.rectangle(thresh, (x, y), (x + w, y + h), (0, 255, 0), 1);
-
-        cv2.imshow("thresh", thresh)
         output = []
 
         for (i, (x, y, w, h)) in enumerate(locs):
@@ -71,5 +70,32 @@ class OCR:
                                            cv2.TM_CCOEFF)
                 (_, score, _, _) = cv2.minMaxLoc(result)
                 scores.append(score)
-            output.append(Card(int(np.argmax(scores)), 0))
+
+            output.append(Card(int(np.argmax(scores)), self.getColor(x, y, w, h)))
         return output
+
+    def getColor(self, x, y, w, h):
+        img = self.selectMyHand()
+
+        roi = img[y:y + h, x:x + w]
+        roi = cv2.resize(roi, (57, 88))
+
+        pc_black = self.getColorRatio(roi, REF_COLOR_RGB_BLACK, 10)
+        pc_blue = self.getColorRatio(roi, REF_COLOR_RGB_BLUE, 10)
+        pc_red = self.getColorRatio(roi, REF_COLOR_RGB_RED, 10)
+        pc_green = self.getColorRatio(roi, REF_COLOR_RGB_GREEN, 10)
+
+        list = [pc_black, pc_green, pc_red, pc_blue]
+        return list.index(max(list))
+
+    def getColorRatio(self, img, color, diff=20):
+        boundaries = [([color[2] - diff, color[1] - diff, color[0] - diff],
+                       [color[2] + diff, color[1] + diff, color[0] + diff])]
+
+        for (lower, upper) in boundaries:
+            lower = np.array(lower)
+            upper = np.array(upper)
+            mask = cv2.inRange(img, lower, upper)
+
+            ratio_color = cv2.countNonZero(mask) / (img.size / 3)
+            return np.round(ratio_color * 100, 2)
